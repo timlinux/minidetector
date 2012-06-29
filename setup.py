0834c395fe087378e0c2a3f45be4d2636b5cd008
Tim Sutton <tim@linfiniti.com>
2012-06-29 09:33:46 +0200

    Packaging fix for minidetector

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='minidetector',
     version='0.1dev',
     packages=['minidetector',],
+    package_data={'minidetector': ['search_strings.txt']},
     license='New BSD License',
     long_description=open('README').read(),
 )
