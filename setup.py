3b3e9627ec4710a52e05bb6405107e4229f75dbf
Tim Sutton <tim@linfiniti.com>
2012-06-29 09:19:10 +0200

    Setup update

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='minidetector',
+    version='0.1dev',
+    packages=['minidetector',],
+    license='New BSD License',
+    long_description=open('README').read(),
+)
